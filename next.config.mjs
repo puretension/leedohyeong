b15aba6a8c5bad33a75ded5e4bfc4e30a63d716c
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2025-03-08 17:37:29 +0900

    Chore: Update project configs and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import("next").NextConfig} */
+import rehypePrism from "@mapbox/rehype-prism";
+import nextMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
 
-export default nextConfig;
+const nextConfig = {
+  images: {
+    domains: ["api.microlink.io", "images.unsplash.com"],
+    formats: ["image/avif", "image/webp"],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  experimental: {
+    mdxRs: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/, // mdx 확장자를 가진 파일을 페이지나 컴포넌트처럼 사용할 수 있도록 설정
+  options: {
+    remarkPlugins: [remarkGfm], // GitHub Flavored Markdown(GFM) 지원
+    rehypePlugins: [rehypePrism], // Markdown 내 코드 하이라이팅 (rehypePlugins)
+  },
+});
+
+export default withMDX(nextConfig); // MDX 설정과 Next.js 설정 합치기
