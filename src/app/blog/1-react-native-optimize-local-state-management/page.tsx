d85f16ccbfc0392cee90b8201cc9a7728ee01a70
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2025-03-10 01:15:09 +0900

    Content: Modify Metadata

--- src/app/blog/1-react-native-optimize-local-state-management/page.tsx
+++ src/app/blog/1-react-native-optimize-local-state-management/page.tsx
@@ -6,20 +6,20 @@ import { DefaultOpenGraph } from "@/app/shared-metadata";
 import Content from "./content.mdx";
 
 export const metadata: Metadata = {
-  title: "리액트 네이티브 - 이도형 블로그",
-  description: "리액트 네이티브 관련 개발 블로그 글 모음",
-  keywords: ["React Native", "모바일 앱 개발"],
+  title: "리액트 네이티브 로컬 상태 관리 최적화",
+  description: "리액트 네이티브 로컬 상태 관리 최적화 방법",
+  keywords: ["React Native", "useContext", "useReducer", "지역 상태 관리"],
   openGraph: {
     ...DefaultOpenGraph,
     url: "https://leedohyeong.com/blog/react-native",
-    title: "리액트 네이티브 - 이도형 블로그",
-    description: "리액트 네이티브 관련 개발 블로그 글 모음",
+    title: "리액트 네이티브 로컬 상태 관리 최적화",
+    description: "리액트 네이티브 로컬 상태 관리 최적화 방법",
     images: [
       {
         url: "https://leedohyeong.com/api/og?path=/blog/react-native",
         width: 1200,
         height: 630,
-        alt: "리액트 네이티브 - 이도형 블로그",
+        alt: "리액트 네이티브 로컬 상태 관리 최적화",
       },
     ],
   },
