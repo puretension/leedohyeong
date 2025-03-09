af22793450d66cb3919c8c810d2da9f28f9b6063
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2025-03-10 01:31:47 +0900

    Content: change openGraph

--- src/app/blog/1-react-native-optimize-local-state-management/page.tsx
+++ src/app/blog/1-react-native-optimize-local-state-management/page.tsx
@@ -6,14 +6,14 @@ import { DefaultOpenGraph } from "@/app/shared-metadata";
 import Content from "./content.mdx";
 
 export const metadata: Metadata = {
-  title: "리액트 네이티브 로컬 상태 관리 최적화",
-  description: "리액트 네이티브 로컬 상태 관리 최적화 방법",
+  title: "Context API + useReducer로 안심하고 로컬 상태 관리하기",
+  description: "Context API + useReducer로 안심하고 로컬 상태 관리하기",
   keywords: ["React Native", "useContext", "useReducer", "지역 상태 관리"],
   openGraph: {
     ...DefaultOpenGraph,
-    url: "https://leedohyeong.com/blog/react-native",
-    title: "리액트 네이티브 로컬 상태 관리 최적화",
-    description: "리액트 네이티브 로컬 상태 관리 최적화 방법",
+    url: "https://leedohyeong.com/blog/1-react-native-optimize-local-state-management/images/thumbnail.png",
+    title: "Context API + useReducer로 안심하고 로컬 상태 관리하기",
+    description: "Context API + useReducer로 안심하고 로컬 상태 관리하기",
     images: [
       {
         url: "/blog/1-react-native-optimize-local-state-management/images/thumbnail.png",
