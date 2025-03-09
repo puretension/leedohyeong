0921860e50526036162a31d8dbea94bf119bcf41
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2025-03-09 21:23:33 +0900

    Chore: add packages and font & css setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import("next").NextConfig} */
 import rehypePrism from "@mapbox/rehype-prism";
 import nextMDX from "@next/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
 import remarkGfm from "remark-gfm";
+
+const options = {
+  theme: "github-dark",
+  keepBackground: true,
+  // 기타 옵션들...
+};
 
 const nextConfig = {
   images: {
@@ -28,7 +35,7 @@ const withMDX = nextMDX({
   extension: /\.mdx?$/, // mdx 확장자를 가진 파일을 페이지나 컴포넌트처럼 사용할 수 있도록 설정
   options: {
     remarkPlugins: [remarkGfm], // GitHub Flavored Markdown(GFM) 지원
-    rehypePlugins: [rehypePrism], // Markdown 내 코드 하이라이팅 (rehypePlugins)
+    rehypePlugins: [rehypePrism, rehypePrettyCode, options], // Markdown 내 코드 하이라이팅 (rehypePlugins)
   },
 });
 
